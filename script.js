1e12da213433d4ca7220f998a895ea6b3b4350d7
karl dudley <karldudley@gmail.com>
2022-10-06 22:42:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let weather = {
+    apikey: "8cd1cb1a93eda480c4a93399d0cd1513",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+this.apikey)
+        .then((r) => r.json())
+        .then((data) => this.displayWeather(data));
+        
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = parseInt(temp) + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (e) {
+    if (e.key == "Enter") {
+        weather.search();
+    }
+    
+});
+
+weather.fetchWeather("stoke");
